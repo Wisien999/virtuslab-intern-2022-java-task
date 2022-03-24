3fc1acebd78bc514d500b576a318942a713aa0a0
K3;3 <41787377+Wisien999@users.noreply.github.com>
2022-03-24 21:58:14 +0100

    Made Spring Boot App work

--- src/main/java/com/virtuslab/internship/rest/ReceiptController.java
+++ src/main/java/com/virtuslab/internship/rest/ReceiptController.java
@@ -3,13 +3,15 @@ package com.virtuslab.internship.rest;
 import com.virtuslab.internship.basket.Basket;
 import com.virtuslab.internship.receipt.Receipt;
 import com.virtuslab.internship.receipt.ReceiptService;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/receipt")
-public class ReceiptController {
-    ReceiptService receiptService;
+public class ReceiptController extends SpringBootServletInitializer {
+    ReceiptService receiptService = new ReceiptService();
 
     @GetMapping
     public ResponseEntity<Receipt> getReceiptFromBasket(@RequestBody Basket basket) {
